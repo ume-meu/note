0b143ca6291e85fe41560a732f53b81b3408c0db
nguyenthiyenchi <nguyenthiyenchi2k3@gmail.com>
2024-07-18 13:14:06 +0700

    note card UI

--- frontend/note-app/src/pages/Home/Home.jsx
+++ frontend/note-app/src/pages/Home/Home.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import NoteCard from "../../components/Cards/NoteCard";
+import { MdAdd } from "react-icons/md";
 
 const Home = () => {
   return (
     <>
-      <Navbar/>
-    </>
-  )
-}
+      <Navbar />
 
-export default Home
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <NoteCard
+            title="Find jobs"
+            date="18th July 2024"
+            content="Looking for intern jobs."
+            tags="#TaskToDo"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
+
+      <button className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10" onClick={() => {}}>
+        <MdAdd className="text-[32px] text-white" />
+      </button>
+    </>
+  );
+};
+
+export default Home;
