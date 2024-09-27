b6ce33304e66a343d92518ccdeced8483718a564
ume <chi.nguyenthiyen.203@gmail.com>
2024-09-27 16:52:48 +0700

    link to Home if enter wrong path

--- frontend/note-app/src/App.jsx
+++ frontend/note-app/src/App.jsx
@@ -10,6 +10,7 @@ const routes = (
       <Route path="/dashboard" exact element={<Home />} />
       <Route path="/login" exact element={<Login />} />
       <Route path="/signup" exact element={<Signup />} />
+      <Route path="/*" exact element={<Home />} />
     </Routes>
   </Router>
 );
